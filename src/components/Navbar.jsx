e851ff77ccb315feef8bff3ec12863038f088e1e
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-07-09 15:42:46 +0200

    Create app icon
    Create app icon and adjust it to screen with responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { Stack, Button } from '@mui/material'
-import Icon from '../assets/vite.svg'
+import Icon from '../assets/icon2.png'
 import { useGlobalContext } from '../utils/context'
 
 function Navbar() {
@@ -18,8 +18,8 @@ function Navbar() {
   }
 
   return(    
-    <Stack direction='row' mb='30px'>
-      <img src={Icon} alt="Icon" />                
+    <Stack direction='row' mb='30px' alignItems='center'>
+      <img src={Icon} alt="Icon" width='40px' height='40px'/>                
       <Button variant='text' color={activeColor.name} onClick={() => comprobarGuardadoPerfil('/')}>INICIO</Button>         
       <Link to='/profile'>
         <Button variant='text' color={activeColor.name} sx={{ boxShadow: location.pathname === '/profile' ? `inset 0 -3px 0 0 ${activeColor.oscuro}`: 'none' }}>PERFIL</Button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Icon from '../assets/vite.svg'
+import Icon from '../assets/icon2.png'
 import { Link } from 'react-router-dom'
 import { Stack, Typography } from '@mui/material'
 import './Home.css'
@@ -6,7 +6,7 @@ import { useGlobalContext } from '../utils/context'
 
 function Home() {
 
-  const { setPerfilActivo, activeColor, setActiveColor, setEsNuevoPerfil, perfiles, setCantidadObjetivo, setMenu, setEsPerfilGuardado } = useGlobalContext()
+  const { setPerfilActivo, setActiveColor, setEsNuevoPerfil, perfiles, setCantidadObjetivo, setMenu, setEsPerfilGuardado } = useGlobalContext()
 
   const crearNuevoPerfil = () => {
     setPerfilActivo('')
@@ -22,31 +22,29 @@ function Home() {
       width: '100%',
       height: '100vh',
       fontSize: '2rem',
-      backgroundColor: `${perfiles.length ? activeColor.claro : '#ADD8E6'}`,
-      color: `${perfiles.length ? activeColor.oscuro : '#1976D2'}`,
+      backgroundColor: 'white',
+      color: '#EB5D45',
       textAlign: 'center',
       padding: '30px'
     }}>
-      {/* <img src={Icon} alt="Icon" style={{width: '10%', margin: '0 auto'}}/> */}
-      <Typography variant='h1' sx={{fontSize: {xs: '2.8rem', lg: '5rem'}}}>¡Bienvenid@ a Fitness App!</Typography>    
+      <img src={Icon} alt="Icon" style={{maxWidth: '320px', maxHeight: '25%', aspectRatio: '1/1', margin: '0 auto'}}/>
+      <Typography variant='h1' sx={{fontSize: {xs: '2.8rem', md: '4rem', lg: '5rem'}}}>¡Bienvenid@ a Fitness App!</Typography>    
       { perfiles.length ? 
         <Stack  className='home' sx={{
           height: '50%',
           fontSize: '2rem',
-          backgroundColor: `${activeColor.claro}`,
-          color: `${activeColor.oscuro}`,
           textAlign: 'center',
           mt: '15px'}}>
-          <Typography variant='p' sx={{fontSize: {xs: '1.7rem', lg: '3rem'}}}>Selecciona tu perfil:</Typography>   
+          <Typography variant='p' sx={{fontSize: {xs: '1.8rem', md: '2.4rem', lg: '3rem'}}}>Selecciona tu perfil:</Typography>   
           <ul>
             {perfiles.map(perfil => {
-              return (<li key={perfil} onClick={() => {setPerfilActivo(perfil); setEsNuevoPerfil(false); setEsPerfilGuardado(true)}}><Link className='link' to='/profile' style={{color: `${activeColor.oscuro}`}}>{perfil}</Link></li>)
+              return (<li key={perfil} onClick={() => {setPerfilActivo(perfil); setEsNuevoPerfil(false); setEsPerfilGuardado(true)}}><Link className='link' to='/profile' style={{color: '#EB5D45'}}>{perfil}</Link></li>)
             })}
           </ul>
-          <Typography variant='p' sx={{fontSize: {xs: '1.7rem', lg: '3rem'}}}>o</Typography>
-          <Typography variant='p'><Link className='link' to='/profile' style={{color: `${activeColor.oscuro}`}} onClick={() => crearNuevoPerfil()}>Crea uno nuevo</Link></Typography>
+          <Typography variant='p' sx={{fontSize: {xs: '1.8rem', md: '2.4rem', lg: '3rem'}}}>o</Typography>
+          <Typography variant='p'><Link className='link' to='/profile' style={{color: '#EB5D45'}} onClick={() => crearNuevoPerfil()}>Crea uno nuevo</Link></Typography>
         </Stack> :
-        <Typography variant='p' className='home-p'><Link className='link' to='/profile' style={{color: '#1976D2'}} onClick={() => crearNuevoPerfil()}>Crea tu perfil</Link></Typography>
+        <Typography variant='p' className='home-p'><Link className='link' to='/profile' style={{color: '#EB5D45'}} onClick={() => crearNuevoPerfil()}>Crea tu perfil</Link></Typography>
       }
       
     </Stack>
