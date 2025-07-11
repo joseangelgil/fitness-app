a0b6626497175104af6e10af7cb5478fb6982784
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-07-11 10:46:49 +0200

    Create file fitnessAppData
    Create the fitnessAppData file to store all the initial data of the app in a different place

--- src/utils/context.jsx
+++ src/utils/context.jsx
@@ -1,20 +1,12 @@
 import { useState, useEffect, createContext, useContext } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { fitnessAppData } from './fitnessAppData'
 
 const AppContext = createContext()
 
 const AppProvider = ({ children }) => {
 
-  const [data, setData] = useState(JSON.parse(localStorage.getItem('data')) || [
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Arroz', kcal: '56', hc: '0', p: '7', g: '15'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Bacon', kcal: '250', hc: '0', p: '12', g: '22'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Espaguetis', kcal: '120', hc: '50', p: '7', g: '11'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Huevo', kcal: '56', hc: '0', p: '7', g: '15'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Melocoton', kcal: '56', hc: '0', p: '7', g: '15'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Melon', kcal: '56', hc: '0', p: '7', g: '15'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Queso', kcal: '210', hc: '2', p: '15', g: '18'},
-    {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Tomate', kcal: '56', hc: '0', p: '7', g: '15'},    
-  ])  
+  const [data, setData] = useState(JSON.parse(localStorage.getItem('data')) || fitnessAppData)  
   const [openFoodFactsData, setOpenFoodFactsData] = useState([])
   const [activeWeekDay, setActiveWeekDay] = useState('Lunes')
   const [activeColor, setActiveColor] = useState({name: 'primary', oscuro: '#1976D2', claro: '#ADD8E6', suave: '#D6ECFA'})  
---
+++ src/utils/fitnessAppData.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const fitnessAppData = [
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Arroz', kcal: '56', hc: '0', p: '7', g: '15'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Bacon', kcal: '250', hc: '0', p: '12', g: '22'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Espaguetis', kcal: '120', hc: '50', p: '7', g: '11'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Huevo', kcal: '56', hc: '0', p: '7', g: '15'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Melocoton', kcal: '56', hc: '0', p: '7', g: '15'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Melon', kcal: '56', hc: '0', p: '7', g: '15'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Queso', kcal: '210', hc: '2', p: '15', g: '18'},
+  {id: uuidv4(), author: 'FitnessApp', image:'', name: 'Tomate', kcal: '56', hc: '0', p: '7', g: '15'}  
+]
